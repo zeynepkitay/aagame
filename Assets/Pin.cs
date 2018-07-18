1807136569e746d6d158d7d02cfa0228e7743604
Zeynep <zeynep.kitay@student.htw-berlin.de>
2018-07-18 06:33:15 +0200

    add project

---
+++ Assets/Pin.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pin : MonoBehaviour {
+
+    private bool isPinned = false;
+
+    public float speed = 20f;
+
+    public Rigidbody2D rb;
+
+    void Update()
+    {
+        if(!isPinned)
+            rb.MovePosition(rb.position + Vector2.up * speed * Time.deltaTime);
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.tag == "Rotator")
+        {
+            //Assigns the game object to the collided
+            //game object to rotate with it.
+            transform.SetParent(col.transform);
+            Score.pinCount++;
+            isPinned = true;
+        }
+
+        else if(col.tag == "Pin")
+        {
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}
---
+++ Assets/Score.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour {
+
+    public static int pinCount = 0;
+
+    public Text text;
+
+    void Start()
+    {
+        pinCount = 0;
+    }
+
+    void Update()
+    {
+        text.text = pinCount.ToString();
+    }
+}
